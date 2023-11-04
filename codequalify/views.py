58f167ec78c8545ce15f8088df41405542716487
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-04 19:37:31 +0900

    add docstring

--- codequalify/views.py
+++ codequalify/views.py
@@ -1,4 +1,10 @@
+"""
+This file contains the views for the codequalify app.
+"""
 from django.shortcuts import render
 
+"""
+This view renders the login page.
+"""
 def login(request):
     return render(request, 'templates/login.html')
