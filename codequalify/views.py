e235dd7845f940a97ab79833e14dc07b878871b6
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-04 19:43:20 +0900

    fix docstring

--- codequalify/views.py
+++ codequalify/views.py
@@ -3,11 +3,14 @@ This file contains the views for the codequalify app.
 """
 from django.shortcuts import render
 
-"""
-This view renders the login page.
-
-:param request: the HTTP request
-:return: the HTTP response
-"""
 def login(request):
+    """
+    This function renders the login page.
+    
+    Parameters:
+        request: the request object
+
+    Returns:
+        The rendered login page.
+    """
     return render(request, 'templates/login.html')
